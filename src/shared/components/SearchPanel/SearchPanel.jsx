7ca5de49b2db05d5d4b3f5606a65b5c32738f765
yixiang <gnoyixiang@gmail.com>
2019-08-07 16:31:42 +0800

    add enter event on search input

--- src/shared/components/SearchPanel/SearchPanel.jsx
+++ src/shared/components/SearchPanel/SearchPanel.jsx
@@ -5,11 +5,22 @@ import './SearchPanel.css';
 const SearchPanel = props => {
     const { searchInput, onSearchInputChange, onSearch, className, ...restProps } = props;
 
+    const onEnter = (evt) => {
+        if(evt.key === 'Enter' && evt.target.value) {
+            onSearch();
+        }
+    }
+
     return (
         <div className={classnames(className, "SearchPanel")} {...restProps}>
             <div className="Searchbar">
                 <span><i className="icofont-search-1"></i></span>
-                <input value={searchInput} onChange={onSearchInputChange} placeholder="Search for job title or company name" />
+                <input
+                    value={searchInput}
+                    onChange={onSearchInputChange}
+                    onKeyPress={onEnter}
+                    placeholder="Search for job title or company name"
+                />
             </div>
             <button className="FilterButton" onClick={onSearch}>
                 Filter results
